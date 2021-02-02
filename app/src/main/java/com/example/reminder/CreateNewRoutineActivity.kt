ceda1f06dfdb634d5cadda1b4e2f3a746dceeb20
kolomaxo <77983068+kolomaxo@users.noreply.github.com>
2021-02-02 13:27:10 +0100

    Added "New routine" view.
    Added frontend for new routine and code for handaling weekys selection.

---
+++ app/src/main/java/com/example/reminder/CreateNewRoutineActivity.kt
@@ -0,0 +1,65 @@
+package com.example.reminder
+
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDate
+
+
+class CreateNewRoutineActivity : AppCompatActivity() {
+    val weekdayButtons = mutableListOf<Button>()
+    val selectedButtonBackground = Color.rgb(233, 30, 99)
+    //val availableButtonBackground = Color.rgb(103, 58, 183)
+    val weekdays = BooleanArray(7)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_new_routine)
+
+        if (weekdayButtons.size == 0) {
+            weekdayButtons.addAll(arrayOf<Button>(
+                    findViewById(R.id.buttonMon),
+                    findViewById(R.id.buttonTue),
+                    findViewById(R.id.buttonWed),
+                    findViewById(R.id.buttonThu),
+                    findViewById(R.id.buttonFri),
+                    findViewById(R.id.buttonSat),
+                    findViewById(R.id.buttonSun)
+            ))
+        }
+
+
+        val weekdayButtonListener = View.OnClickListener {
+            val tmp = it.getBackground()
+            with(it as Button) {
+                val clickedWeekdayIndex = weekdayButtons.indexOf(this)
+                flipWeekdayState(clickedWeekdayIndex)
+            }
+        }
+
+        for (button in weekdayButtons) {
+            button.setOnClickListener(weekdayButtonListener)
+            //button.background.setTint(availableButtonBackground)
+        }
+
+        val  todayWeekdayIndex = LocalDate.now().dayOfWeek.value - 1
+        flipWeekdayState(todayWeekdayIndex)
+    }
+
+
+    fun flipWeekdayState(weekdayIndex: Int) = if (!weekdays[weekdayIndex]) {
+        val tmp = weekdayButtons[weekdayIndex].background
+        weekdayButtons[weekdayIndex].background.setTint(selectedButtonBackground)
+        weekdays[weekdayIndex] = true
+    } else {
+        val a = TypedValue()
+        theme.resolveAttribute(R.attr.colorPrimary, a, true)
+        weekdayButtons[weekdayIndex].background.setTint(a.data)  // works only for colours, not resources. But it do
+        weekdays[weekdayIndex] = false
+    }
+}
--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.reminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps (+)*/
+    fun addNewRoutine(view: View) {
+        val intent = Intent(this, CreateNewRoutineActivity::class.java)
+        startActivity(intent)
     }
 }
